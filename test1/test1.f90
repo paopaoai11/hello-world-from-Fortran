1d0f7de0822529a102830506acb0f96571c45916
Shao <912010561@qq.com>
2020-01-27 17:00:41 +0800

    Add files via upload

---
+++ test1/test1.f90
@@ -0,0 +1,26 @@
+!  test1.f90 
+!
+!  FUNCTIONS:
+!  test1 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: test1
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program test1
+
+    implicit none
+
+    ! Variables
+
+    ! Body of test1
+    print *, 'Hello World'
+    read *
+
+    end program test1
+
